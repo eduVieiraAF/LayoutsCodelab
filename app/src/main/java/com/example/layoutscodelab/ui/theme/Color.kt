1c0b7c8c90d82d1e27da2c5e50ff165f9c20e220
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-27 08:45:41 -0300

    Modifier
    Working on initial outlook

---
+++ app/src/main/java/com/example/layoutscodelab/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.layoutscodelab.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xffc9e0dd)
+val Teal700 = Color(0xff007765)
