9d92b851b833931303c59d4d30c090897fdf1509
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-27 10:36:57 -0300

    Lazy List
    Using LazyColumn

--- app/src/main/java/com/example/layoutscodelab/MainActivity.kt
+++ app/src/main/java/com/example/layoutscodelab/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.layoutscodelab
 
+import android.app.Notification
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -18,7 +23,10 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.layoutscodelab.ui.theme.LayoutsCodelabTheme
 
@@ -155,3 +163,31 @@ fun SimpleListPreview() {
 }
 */
 // endRegion
+
+@Composable
+fun LazyList(){
+    // We save the scrolling position with this state that can also
+    // be used to programmatically scroll the list
+    val scrollState = rememberLazyListState()
+
+    LazyColumn(
+        state = scrollState,
+        modifier = Modifier.padding(8.dp)
+    ){
+        items(100){
+            Text(
+                "Item → $it",
+                color = MaterialTheme.colors.secondaryVariant
+            )
+            Spacer(modifier = Modifier.padding(2.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LazyListPreview(){
+    LayoutsCodelabTheme {
+        LazyList()
+    }
+}
