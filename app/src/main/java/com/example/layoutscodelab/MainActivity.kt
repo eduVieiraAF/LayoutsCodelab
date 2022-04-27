f4fdfabd655ec520ec758830d0252abe1738dcc6
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-27 12:47:17 -0300

    Layout Modifier

--- app/src/main/java/com/example/layoutscodelab/MainActivity.kt
+++ app/src/main/java/com/example/layoutscodelab/MainActivity.kt
@@ -21,10 +21,14 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.layoutscodelab.ui.theme.LayoutsCodelabTheme
@@ -192,6 +196,8 @@ fun LazyListPreview(){
 }*/
 // endRegion
 
+
+//region → LazyColumn with Image by Coil.
 @Composable
 fun ImageListItem(index: Int) {
     Row(verticalAlignment = Alignment.CenterVertically) {
@@ -244,11 +250,49 @@ fun ScrollingList() {
     }
 }
 
+/*
 @Preview(showBackground = true)
-@Composable
+ @Composable
 fun ImageListItemPreview() {
     LayoutsCodelabTheme {
         ScrollingList()
     }
 }
+*/
+//endRegion
 
+fun Modifier.firstBaselineTop(
+    firstBaselineTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        // Check the composable has a first baseline
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        // Height of the composable with padding - first baseline
+        val placeableY = firstBaselineTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            // Where the composable gets placed
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Preview(showBackground = true)
+@Composable
+fun TextWithPaddingToBaselinePreview(){
+    LayoutsCodelabTheme {
+        Text("hello, World!", Modifier.firstBaselineTop(32.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutsCodelabTheme {
+        Text("Hello, World!", Modifier.padding(top = 32.dp))
+    }
+}
